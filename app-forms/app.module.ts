2f3252281d8ec0e42ac743edcf0cb17eb7c6ac99
Asaad Saad <asaad@outlook.com>
2021-11-12 09:47:30 -0600

    Update app.module.ts

--- app-forms/app.module.ts
+++ app-forms/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// use FormsModule Only when using Template Driven Forms
-import { FormsModule } from '@angular/forms';
+
 // use ReactiveFormsModule Only when using Template Driven Forms
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -16,7 +15,6 @@ import { DataDrivenComponent } from "./data-driven/data-driven.component";
   // to use Forms we must have FormsModule OR ReactiveFormsModule Here
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
